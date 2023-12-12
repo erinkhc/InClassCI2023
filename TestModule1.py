6a2d91ff37437997dc92d38aea692e0ccc8f41de
erinkhc <erin92012@gmail.com>
2023-12-11 18:08:58 -0800

    test

--- TestModule1.py
+++ TestModule1.py
@@ -3,9 +3,9 @@ from mod1 import addition as md1
 
 
 class TestAdd(unittest.TestCase):
-
     def test_addition(self):
-        self.assertEqual(md1(1,2),3)
-        self.assertEqual(md1(1,5),6)
-     
+        self.assertEqual(md1(1, 2), 3)
+        self.assertEqual(md1(1, 5), 6)
+
+
 unittest.main()
